f38874c782316f730bb44268d6b87ee277137626
Fahad <Shahriarnso96619661@gmail.com>
2026-02-23 14:05:41 +0600

    Fast api request_testing

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 from fastapi import FastAPI
-from pydantic import BaseModel,EmailStr
-import asyncio
-import time
+from pydantic import BaseModel
 
 
+
+app=FastAPI()
 class User(BaseModel):
     name:str
     age:int
-    email:EmailStr
 
-app=FastAPI()
-
-
-@app.get("/multiply/{a}/{b}")
-def multyply(a:int,b:int):
-    return {a*b}
-
-def add(ab,f):
-    return {"Fahad"}
-def asylum(shear_brute):
-    return ("We must endure the")
+@app.post("/users")
+def create_user(user:User):
+    return {"sended_name":user.name,"sended_age":user.age}
+   
 
 
-async def george(a,b):
-    await time.sleep(6)
-    return (a+b)
 
-async def get():
+
+    
  
 
 
