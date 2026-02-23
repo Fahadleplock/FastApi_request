6d60bf9ee384d1915a16659b0e5b94d62d663d05
Fahad <Shahriarnso96619661@gmail.com>
2026-02-23 15:30:37 +0600

    Exercise problem

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-
+from pydantic import BaseModel,EmailStr,Field,field_validator
+import re
 app=FastAPI()
-class User(BaseModel):
-    name:str
-    age:int
 
-@app.post("/users")
-def create_user(user:User):
-    return {"sended_name":user.name,"sended_age":user.age}
-   
+class UserRegister(BaseModel):
+    username:str=Field(...,min_length=5,max_length=15)
+    email:EmailStr
+    password:str
+    age:int=Field(...,ge=18,le=100)
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls,value):
+        if not re.search(r"\d",value):
+            raise ValueError("Password must contain atleast 1 number")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("password should contain ! special character")
+        return value
+@app.post("/register")
+def register(user: UserRegister):
+    return {
+        "message": "User registered successfully",
+        "user": user
+    }    
+        
+        
+        
+        
+        
+
+
+
 
 
 
